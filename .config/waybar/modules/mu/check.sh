30b058b76af081552c70fe4a0f7068e1be7e619c
Méril Pilon <me@mpsq.org>
2022-01-03 12:18:34 +0000

    scripts: restructure folders + add checks + use bash shell

--- .config/waybar/modules/mu/check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-count=$(mu msgs-count --query='flag:unread AND NOT flag:trashed')
-
-if [[ count -gt 0 ]]; then
-  echo "{\"text\":\"\uf0e0 $count\",\"class\":\"new\"}"
-else
-  echo "{\"text\":\"\"}"
-fi
-
