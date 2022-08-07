fd5f4ba9afcdc4975041b095d16ee00fa707b05d
Méril Pilon <me@mpsq.org>
2022-08-07 15:00:43 +0100

    emacs: pin lsp-mode & doom-themes pkg

--- .config/doom-emacs/packages.el
+++ .config/doom-emacs/packages.el
@@ -1,2 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
+(package! lsp-mode :pin "92004824f2aafce7aedf202f747258cc433afbcc")
+(package! doom-themes :pin "5f96ee83e9734587bf3e8ec61d4f067b43d9c26e")
