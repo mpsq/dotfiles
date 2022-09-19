5e031c1a4913f35ceb0c582acc9c2908ceb8bab9
Méril Pilon <me@mpsq.org>
2022-09-19 13:56:08 +0100

    emacs: unpin lsp-mode & rm rego-mode

--- .config/doom-emacs/packages.el
+++ .config/doom-emacs/packages.el
@@ -1,5 +1,3 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
-(package! lsp-mode :pin "92004824f2aafce7aedf202f747258cc433afbcc")
 (package! doom-themes :pin "5f96ee83e9734587bf3e8ec61d4f067b43d9c26e")
-(package! rego-mode)
