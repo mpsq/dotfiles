e8c327c9b4df371b421c3ca3b1c1378757aad51b
Méril Pilon <me@mpsq.org>
2022-10-04 18:17:27 +0100

    emacs: unpin lsp

--- .config/doom-emacs/packages.el
+++ .config/doom-emacs/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
-(package! lsp-mode :pin "bbce14c74f1697672e871d81d3cc20f294b2732f")
+(unpin! lsp-mode)
 (package! doom-themes :pin "5f96ee83e9734587bf3e8ec61d4f067b43d9c26e")
