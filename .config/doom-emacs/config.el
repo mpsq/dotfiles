2151748905d4a20fad74f94b62efbd95c6cacc60
Méril Pilon <me@mpsq.org>
2026-03-11 12:34:34 +0000

    chore(doom): change theme to monokai-spectrum

--- .config/doom-emacs/config.el
+++ .config/doom-emacs/config.el
@@ -1,7 +1,7 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;; Doom config
-(setq doom-theme 'doom-monokai-ristretto
+(setq doom-theme 'doom-monokai-spectrum
       doom-font (font-spec :family "Iosevka Fixed SS16" :size 13)
       doom-big-font (font-spec :family "Iosevka Fixed SS16" :size 14)
       doom-variable-pitch-font (font-spec :family "Inter" :size 13)
