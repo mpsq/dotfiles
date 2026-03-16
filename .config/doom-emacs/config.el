1b7e85082291eaac85d63afbc2119e685bfa666c
Méril Pilon <me@mpsq.org>
2026-03-16 23:29:46 +0000

    chore(emacs): use abs font size

--- .config/doom-emacs/config.el
+++ .config/doom-emacs/config.el
@@ -2,9 +2,9 @@
 
 ;; Doom config
 (setq doom-theme 'doom-monokai-spectrum
-      doom-font (font-spec :family "Iosevka Fixed SS16" :size 13.0)
-      doom-big-font (font-spec :family "Iosevka Fixed SS16" :size 16.0)
-      doom-variable-pitch-font (font-spec :family "Inter" :size 13.0)
+      doom-font (font-spec :family "Iosevka Fixed SS16" :size 13)
+      doom-big-font (font-spec :family "Iosevka Fixed SS16" :size 16)
+      doom-variable-pitch-font (font-spec :family "Inter" :size 13)
       doom-symbol-font (font-spec :family "Liberation Mono")
       doom-serif-font (font-spec :family "Droid Serif"))
 (setq confirm-kill-emacs nil)
